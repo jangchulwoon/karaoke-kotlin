93425a44474e6bcb4f1b2346cc965f1288d5871e
jangchulwoon <lusiue@gmail.com>
2020-07-06 13:45:15 +0900

    init controller

--- src/main/kotlin/com/karaoke/KaraokeApplication.kt
+++ src/main/kotlin/com/karaoke/KaraokeApplication.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class KaraokeApplication
 
-//https://spring.io/guides/tutorials/spring-boot-kotlin/
 fun main(args: Array<String>) {
     runApplication<KaraokeApplication>(*args)
 }
