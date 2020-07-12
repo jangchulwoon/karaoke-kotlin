3ce740aaf16225e21ae5a08a3caa5e3f7483ec63
jangchulwoon <lusiue@gmail.com>
2020-07-12 20:07:11 +0900

    remove empty test file

--- src/main/kotlin/com/karaoke/service/KaraokeParser.kt
+++ src/main/kotlin/com/karaoke/service/KaraokeParser.kt
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service
 class KaraokeParser {
     fun searchSong(type: SearchType, keyword: String) {
         TODO("implement")
+        // 요 코드는 tdd 로 해볼까
     }
 
 }
